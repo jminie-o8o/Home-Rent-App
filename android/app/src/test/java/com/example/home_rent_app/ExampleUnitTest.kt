eefbfdf43b9175502136df972237d3ed43b5beda
hwj <jminie@naver.com>
2022-07-27 14:22:54 +0900

    feat: ktlint 적용

--- android/app/src/test/java/com/example/home_rent_app/ExampleUnitTest.kt
+++ android/app/src/test/java/com/example/home_rent_app/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.home_rent_app
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
