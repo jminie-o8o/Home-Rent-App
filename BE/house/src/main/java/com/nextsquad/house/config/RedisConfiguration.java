1963d43ec13c1fd12c398f1ff602392752d6cfc1
Senglee Choi <tmdfl1027@gmail.com>
2022-07-28 14:22:00 +0900

    Feat: 레디스/JWT 설정
    레디스/JWT 설정을 위한 의존성 및 RedisConfiguration 작성

--- BE/house/src/main/java/com/nextsquad/house/config/RedisConfiguration.java
+++ BE/house/src/main/java/com/nextsquad/house/config/RedisConfiguration.java
@@ -5,17 +5,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
-    @Value("${spring.redis.host")
-    private String host;
-
-    @Value("${spring.redis.port")
-    private int port;
+//    @Value("${spring.redis.host}")
+//    private String host;
+//
+//    @Value("${spring.redis.port}")
+//    private int port;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(host, port);
+        return new LettuceConnectionFactory("127.0.0.1", 6379);
     }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setDefaultSerializer(RedisSerializer.json());
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+
 }
