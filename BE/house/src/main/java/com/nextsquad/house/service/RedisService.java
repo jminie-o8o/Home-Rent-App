a5698e49ea3fb80c90f113d31593ac145925ad9e
leekm0310 <leekm310@gmail.com>
2022-07-29 16:25:27 +0900

    Feat: 로그아웃 기능 초기 구현 완료
    - TODO: 하드코딩 된 부분 리팩토링 하기
    - 리프레시 토큰을 지우고 액세스 토큰(15분 후 만료)을 레디스에 저장했다. 이후에 인터셉터 처리하면 됨.

--- BE/house/src/main/java/com/nextsquad/house/service/RedisService.java
+++ BE/house/src/main/java/com/nextsquad/house/service/RedisService.java
@@ -5,18 +5,26 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @RequiredArgsConstructor
 @Service
 public class RedisService {
     private final RedisTemplate<String, String> redisTemplate;
 
     public String getRefreshToken(String accountId){
-
         ValueOperations<String, String> values = redisTemplate.opsForValue();
         return values.get(accountId);
     }
-
     public void save(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
+    }
+
+    public void save(String key, String value, int time) {
+        redisTemplate.opsForValue().set(key, value, Duration.ofMinutes(time));
+    }
+
+    public void delete(String key){
+        redisTemplate.delete(key);
     }
 }
