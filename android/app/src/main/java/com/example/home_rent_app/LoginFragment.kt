c48937efcefc7543b751afb28f18c3a91b18d0d6
hwj <jminie@naver.com>
2022-08-01 15:36:01 +0900

    feat: 카카오로그인 구현중

--- android/app/src/main/java/com/example/home_rent_app/LoginFragment.kt
+++ android/app/src/main/java/com/example/home_rent_app/LoginFragment.kt
@@ -1,5 +1,7 @@
 package com.example.home_rent_app
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.home_rent_app.databinding.FragmentLoginBinding
+import okhttp3.HttpUrl
 
 class LoginFragment : Fragment() {
 
@@ -20,5 +23,27 @@ class LoginFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnKakaoLogin.setOnClickListener {
+            kakaoLogin()
+        }
+    }
+
+    private fun kakaoLogin() {
+        val httpUrl = HttpUrl.Builder()
+            .scheme("https")
+            .host("kauth.kakao.com")
+            .addPathSegment("oauth")
+            .addPathSegment("authorize")
+            .addQueryParameter("client_id", "9dc5e51153cd29428199781510c17a32")
+            .addQueryParameter("redirect_uri", "http://jipnumgigi.com/login/oauth/callback")
+            .addQueryParameter("response_type", "code")
+            .build()
+
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(httpUrl.toString()))
+        startActivity(intent)
     }
 }
