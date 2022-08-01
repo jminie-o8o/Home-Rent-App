7c4dd047eb7233373536bdf2a96d6c01275be93e
hwj <jminie@naver.com>
2022-08-01 11:14:32 +0900

    feat: 의존성 추가 및 Jetpack Navigation 추가

---
+++ android/app/src/main/java/com/example/home_rent_app/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.home_rent_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.home_rent_app.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+}
