8f8e3dbdaa09800d5fec25db09758cbcd93f6e45
ese111 <feung94@gmail.com>
2022-08-01 17:16:05 +0900

    fixed tracked files

---
+++ android/app/src/androidTest/java/com/example/home_rent_app/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package com.example.home_rent_app
+
+import org.junit.runner.RunWith
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.home_rent_app", appContext.packageName)
+    }
+}
