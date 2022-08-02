3ed15553232c95e5976e28c371bbaec34d69a6cb
leekm0310 <leekm310@gmail.com>
2022-08-02 14:58:48 +0900

    Feat: 사용자 정보 수정 기능 구현
    - 기획서에 맞게 gender 필드 추가
    - 유저 정보를 업데이트 할 수 있도록 User.modifyInfo에서 UserInfoDto 정보 이용

--- BE/house/src/main/java/com/nextsquad/house/controller/UserController.java
+++ BE/house/src/main/java/com/nextsquad/house/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.nextsquad.house.controller;
 
+import com.nextsquad.house.dto.GeneralResponseDto;
+import com.nextsquad.house.dto.UserInfoDto;
 import com.nextsquad.house.dto.UserResponseDto;
 import com.nextsquad.house.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,6 +18,11 @@ public class UserController {
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponseDto> getUserInfo(@PathVariable long userId) {
         return ResponseEntity.ok(userService.getUserInfo(userId));
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<GeneralResponseDto> modifyUserInfo(@PathVariable long userId, @RequestBody UserInfoDto userInfoDto) {
+        return ResponseEntity.ok(userService.modifyUserInfo(userId, userInfoDto));
     }
 
 }
--- BE/house/src/main/java/com/nextsquad/house/service/UserService.java
+++ BE/house/src/main/java/com/nextsquad/house/service/UserService.java
@@ -1,12 +1,17 @@
 package com.nextsquad.house.service;
 
 import com.nextsquad.house.domain.user.User;
+import com.nextsquad.house.dto.GeneralResponseDto;
+import com.nextsquad.house.dto.UserInfoDto;
 import com.nextsquad.house.dto.UserResponseDto;
 import com.nextsquad.house.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserService {
 
@@ -15,5 +20,11 @@ public class UserService {
     public UserResponseDto getUserInfo(Long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("없는 유저 입니다."));
         return UserResponseDto.from(user);
+    }
+
+    public GeneralResponseDto modifyUserInfo(Long id, UserInfoDto userInfoDto){
+        User user = userRepository.findById(id).orElseThrow();
+        user.modifyInfo(userInfoDto);
+        return new GeneralResponseDto(200, "정보가 수정되었습니다");
     }
 }
