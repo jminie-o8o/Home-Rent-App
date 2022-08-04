67eeefe9e3d4ded585ce5bdb149aa6ef0051e6f2
hwj <jminie@naver.com>
2022-08-04 16:54:24 +0900

    feat: 미디어 권한 설정 구현 및 앨범에서 선택한 이미지로 프로필 설정 구현

--- android/app/src/main/java/com/example/home_rent_app/ui/loginprofile/LoginProfileFragment.kt
+++ android/app/src/main/java/com/example/home_rent_app/ui/loginprofile/LoginProfileFragment.kt
@@ -1,13 +1,24 @@
 package com.example.home_rent_app.ui.loginprofile
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import coil.load
 import com.example.home_rent_app.R
 import com.example.home_rent_app.databinding.FragmentLoginProfileBinding
+import com.example.home_rent_app.ui.LoginActivity
+import java.util.jar.Manifest
 
 class LoginProfileFragment : Fragment() {
 
@@ -17,7 +28,61 @@ class LoginProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login_profile, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_login_profile, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivLoginProfile.setOnClickListener {
+            selectGallery()
+        }
+    }
+
+    private val imageResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val imageUri = result.data?.data
+                imageUri?.let {
+                    // 서버 업로드를 위해 파일 형태로 변환
+                    binding.ivLoginProfile.load(it)
+                }
+            }
+        }
+
+    private fun selectGallery() {
+        val writePermission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        val readPermission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
+        if (writePermission == PackageManager.PERMISSION_DENIED || readPermission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(
+                activity as Activity,
+                arrayOf(
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ),
+                REQ_GALLERY
+            )
+        } else {
+            // 권한이 있는 경우 갤러리 실행
+            val intent = Intent(Intent.ACTION_PICK)
+            // intent 의 data 와 type 을 동시에 설정하는 메서드
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                "image/*"
+            )
+            imageResult.launch(intent)
+        }
+    }
+
+    companion object {
+        const val REQ_GALLERY = 1
     }
 }
