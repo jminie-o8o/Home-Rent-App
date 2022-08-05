16d8243f86f5d41cea3861c8057d12d7d63516d8
leekm0310 <leekm310@gmail.com>
2022-08-05 11:51:43 +0900

    Refactor: 중간테이블 repository 이름 변경(FacilityInHomeRepository)
    - RentArticleFacilityRepository 에서 FacilityInHomeRepository로 이름 변경

--- BE/house/src/main/java/com/nextsquad/house/repository/FacilityInHomeRepository.java
+++ BE/house/src/main/java/com/nextsquad/house/repository/FacilityInHomeRepository.java
@@ -3,5 +3,5 @@ package com.nextsquad.house.repository;
 import com.nextsquad.house.domain.house.RentArticleFacility;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RentArticleFacilityRepository extends JpaRepository<RentArticleFacility, Long> {
+public interface FacilityInHomeRepository extends JpaRepository<RentArticleFacility, Long> {
 }
