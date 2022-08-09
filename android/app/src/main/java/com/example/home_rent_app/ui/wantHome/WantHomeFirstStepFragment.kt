625930967b87f708f032b9348837aee1ab054763
hwj <jminie@naver.com>
2022-08-09 16:37:59 +0900

    feat: 입주 희망일, 퇴거 희망일 DatePicker 이용해 날짜 선택 구현

--- android/app/src/main/java/com/example/home_rent_app/ui/wantHome/WantHomeFirstStepFragment.kt
+++ android/app/src/main/java/com/example/home_rent_app/ui/wantHome/WantHomeFirstStepFragment.kt
@@ -1,14 +1,25 @@
 package com.example.home_rent_app.ui.wantHome
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.home_rent_app.R
 import com.example.home_rent_app.databinding.FragmentWantHomeFirstStepBinding
 import com.example.home_rent_app.ui.WantHomeActivity
+import com.example.home_rent_app.util.RangeValidator
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class WantHomeFirstStepFragment : Fragment() {
 
@@ -24,9 +35,11 @@ class WantHomeFirstStepFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         goBack()
+        showDatePicker()
     }
 
     private fun goBack() {
@@ -36,7 +49,62 @@ class WantHomeFirstStepFragment : Fragment() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun showDatePicker() {
+        binding.btnGoIn.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                .setCalendarConstraints(limitRange().build())
+                .setTitleText("입주 희망일")
+                .build()
+            datePicker.show(childFragmentManager, "date_picker")
+            datePicker.addOnPositiveButtonClickListener {
+                val date =
+                    SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(it)
+                binding.btnGoIn.text = date
+            }
+        }
+        binding.btnGoOut.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                .setCalendarConstraints(limitRange().build())
+                .setTitleText("퇴거 희망일")
+                .build()
+            datePicker.show(childFragmentManager, "date_picker")
+            datePicker.addOnPositiveButtonClickListener {
+                val date =
+                    SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(it)
+                binding.btnGoOut.text = date
+            }
+        }
+    }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun limitRange(): CalendarConstraints.Builder {
+
+        val constraintsBuilderRange = CalendarConstraints.Builder()
+
+        val calendarStart: Calendar = GregorianCalendar.getInstance()
+        val calendarEnd: Calendar = GregorianCalendar.getInstance()
+
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ISO_DATE
+        val formatted = current.format(formatter)
+        val currentDate = formatted.split("-")
+
+        calendarStart.set(
+            currentDate[0].toInt(),
+            currentDate[1].toInt() - 1,
+            currentDate[2].toInt() - 1
+        )
+        calendarEnd.set(2999, 12, 31)
+
+        val minDate = calendarStart.timeInMillis
+        val maxDate = calendarEnd.timeInMillis
+
+        constraintsBuilderRange.setStart(minDate)
+        constraintsBuilderRange.setEnd(maxDate)
+
+        constraintsBuilderRange.setValidator(RangeValidator(minDate, maxDate))
+
+        return constraintsBuilderRange
     }
 }
