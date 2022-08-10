ac12f12f9b35b33dedc6468f3b080e24fde2070f
Senglee Choi <tmdfl1027@gmail.com>
2022-08-10 14:31:39 +0900

    Feat: 중간 테이블 Repository JPQL 정의
    - Spring data JPA에서 제공하는 findAllBy~() 메서드의 DB 통신이 많아 한 번에 가져오는 쿼리로 변경.

--- BE/house/src/main/java/com/nextsquad/house/repository/FacilityInHomeRepository.java
+++ BE/house/src/main/java/com/nextsquad/house/repository/FacilityInHomeRepository.java
@@ -1,7 +1,13 @@
 package com.nextsquad.house.repository;
 
+import com.nextsquad.house.domain.house.RentArticle;
 import com.nextsquad.house.domain.house.RentArticleFacility;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FacilityInHomeRepository extends JpaRepository<RentArticleFacility, Long> {
+    @Modifying
+    @Query("delete from RentArticleFacility f where f.rentArticle = :rentArticle")
+    int deleteAllByRentArticle(RentArticle rentArticle);
 }
