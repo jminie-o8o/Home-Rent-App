0300185d6d0df24577da05dc56b79b1eb96588b0
Senglee Choi <tmdfl1027@gmail.com>
2022-08-12 12:27:39 +0900

    Feat: 양수 글 북마크 삭제 기능 구현
    - 중간 테이블인 WantedArticleBookmarkRepository에서 해당 북마크 엔티티를 삭제처리하는 방식으로 구현

--- BE/house/src/main/java/com/nextsquad/house/repository/WantedArticleBookmarkRepository.java
+++ BE/house/src/main/java/com/nextsquad/house/repository/WantedArticleBookmarkRepository.java
@@ -1,7 +1,17 @@
 package com.nextsquad.house.repository;
 
+import com.nextsquad.house.domain.house.RentArticle;
+import com.nextsquad.house.domain.house.RentArticleBookmark;
+import com.nextsquad.house.domain.house.WantedArticle;
 import com.nextsquad.house.domain.house.WantedArticleBookmark;
+import com.nextsquad.house.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface WantedArticleBookmarkRepository extends JpaRepository<WantedArticleBookmark, Long> {
+    List<WantedArticleBookmark> findByUser(User user);
+
+    Optional<WantedArticleBookmark> findByUserAndWantedArticle(User user, WantedArticle wantedArticle);
 }
