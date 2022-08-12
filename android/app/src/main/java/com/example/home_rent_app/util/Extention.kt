74c15653bef95c4742a10b11a5a098e659e586ab
hwj <jminie@naver.com>
2022-08-12 14:09:04 +0900

    feat: Flow의 throttleFirst 확장함수를 구현하여 좋아요 버튼 다중 클릭 방지 구현

--- android/app/src/main/java/com/example/home_rent_app/util/Extention.kt
+++ android/app/src/main/java/com/example/home_rent_app/util/Extention.kt
@@ -1,11 +1,15 @@
 package com.example.home_rent_app.util
 
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 fun <T> Fragment.collectStateFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
@@ -22,4 +26,15 @@ fun <T> AppCompatActivity.collectStateFlow(flow: Flow<T>, collect: suspend (T) -
             flow.collect(collect)
         }
     }
+}
+
+fun View.setLikeClickEvent(
+    uiScope: CoroutineScope,
+    windowDuration: Long = 3000L,
+    onClick: () -> Unit
+) {
+    clickToFlow()
+        .throttleFirst(windowDuration)
+        .onEach { onClick.invoke() }
+        .launchIn(uiScope)
 }
