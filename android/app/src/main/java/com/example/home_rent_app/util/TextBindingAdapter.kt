3857b8797599ed77ef4a79ee7c3c4632bf32bb7a
hwj <jminie@naver.com>
2022-08-21 16:27:26 +0900

    feat: Binding Adpater를 이용하여 입주일, 퇴거일을 원하는 포맷으로 변경

--- android/app/src/main/java/com/example/home_rent_app/util/TextBindingAdapter.kt
+++ android/app/src/main/java/com/example/home_rent_app/util/TextBindingAdapter.kt
@@ -7,10 +7,32 @@ import com.example.home_rent_app.R
 import java.text.SimpleDateFormat
 
 @SuppressLint("SimpleDateFormat")
-@BindingAdapter("formattingDate")
-fun dateFormat(view: TextView, stringDate: String) {
-    val format = SimpleDateFormat(view.context.getString(R.string.date_format))
-    val date = format.parse(stringDate)
-    val result = date?.let { calculateTime(date) } ?: ""
+@BindingAdapter("formattingTime")
+fun timeFormat(view: TextView, stringTime: String) {
+    val format = SimpleDateFormat(view.context.getString(R.string.time_format))
+    val time = format.parse(stringTime)
+    val result = time?.let { calculateTime(time) } ?: ""
     view.text = result
+}
+
+@BindingAdapter("formattingStartDate")
+fun startDateFormat(view: TextView, stringDate: String) {
+    val month: String = if (stringDate.slice(5..6).startsWith("0")) stringDate[6].toString()
+    else stringDate.slice(5..6)
+
+    val day: String = if (stringDate.slice(8..9).startsWith("0")) stringDate[9].toString()
+    else stringDate.slice(8..9)
+
+    view.text = view.context.getString(R.string.start_date_format, month, day)
+}
+
+@BindingAdapter("formattingEndDate")
+fun endDateFormat(view: TextView, stringDate: String) {
+    val month: String = if (stringDate.slice(5..6).startsWith("0")) stringDate[6].toString()
+    else stringDate.slice(5..6)
+
+    val day: String = if (stringDate.slice(8..9).startsWith("0")) stringDate[9].toString()
+    else stringDate.slice(8..9)
+
+    view.text = view.context.getString(R.string.end_date_format, month, day)
 }
