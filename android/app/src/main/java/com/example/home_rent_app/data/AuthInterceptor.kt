b5485c0b6ceb5c05d06040b2d5d3243b83819bf5
ese111 <feung94@gmail.com>
2022-08-25 23:49:58 +0900

    fix: refresh token 로직 수정 Authenticator를 이용해서 refresh token 요청하도록 변경

--- android/app/src/main/java/com/example/home_rent_app/data/AuthInterceptor.kt
+++ android/app/src/main/java/com/example/home_rent_app/data/AuthInterceptor.kt
@@ -7,8 +7,7 @@ import com.example.home_rent_app.util.CoroutineException
 import com.example.home_rent_app.util.logger
 import io.getstream.chat.android.client.utils.toResult
 import kotlinx.coroutines.*
-import okhttp3.Interceptor
-import okhttp3.Response
+import okhttp3.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -17,7 +16,7 @@ class AuthInterceptor @Inject constructor(
     private val appSession: AppSession,
     private val tokenRepository: TokenRepository,
     private val refreshRepository: RefreshRepository
-) : Interceptor {
+) : Interceptor, Authenticator {
 
     override fun intercept(chain: Interceptor.Chain): Response {
 
@@ -29,36 +28,26 @@ class AuthInterceptor @Inject constructor(
         jwt?.let {
             requestBuilder.addHeader(
                 "access-token",
-                it.accessToken.tokenCode
+                ""
             ) // 추후 수정
         }
 
-        val response = chain.proceed(requestBuilder.build())
+        return chain.proceed(requestBuilder.build())
+    }
 
-        val ceh = CoroutineExceptionHandler { _, throwable ->
-            logger("refresh token error : ${CoroutineException.checkThrowable(throwable).errorMessage}")
+    override fun authenticate(route: Route?, response: Response): Request {
+        val list = runBlocking(Dispatchers.IO) {
+            val jwt = refreshRepository.refreshToken()
+            val tokenCode = listOf(jwt.accessToken.tokenCode, jwt.refreshToken.tokenCode)
+            tokenRepository.saveToken(tokenCode)
+            tokenRepository.setAppSession(tokenCode)
+            tokenCode
         }
-
-        if (response.code == 401) {
-            val refreshBuilder = chain.request()
-                .newBuilder()
-            runBlocking {
-                CoroutineScope(Job() + ceh).launch {
-                    val list = withContext(Dispatchers.IO) {
-                        val token = refreshRepository.refreshToken()
-                        listOf(token.accessToken.tokenCode, token.refreshToken.tokenCode)
-                    }
-                    tokenRepository.saveToken(list)
-                    tokenRepository.setAppSession(list)
-                    refreshBuilder.addHeader(
-                        "access-token",
-                        list[0]
-                    ) // 추후 수정
-                }.join()
-            }
-            return chain.proceed(refreshBuilder.build())
-        }
-        return response
+        return response.request
+            .newBuilder()
+            .removeHeader("access-token")
+            .addHeader("access-token", list[0])
+            .build()
     }
 
 }
