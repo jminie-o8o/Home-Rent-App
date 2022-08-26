57752ac2457cb2db5503129c21e006d2cdff2f46
Senglee Choi <tmdfl1027@gmail.com>
2022-08-26 12:13:24 +0900

    Fix: 기존 custom exception 메시지 추가
    - 누락되어 있던 BookmarkNotFoundException의 메시지 추가
    - OauthClientNotFoundException의 메시지 변경

--- BE/house/src/main/java/com/nextsquad/house/exception/BookmarkNotFoundException.java
+++ BE/house/src/main/java/com/nextsquad/house/exception/BookmarkNotFoundException.java
@@ -2,6 +2,6 @@ package com.nextsquad.house.exception;
 
 public class BookmarkNotFoundException extends RuntimeException{
     public BookmarkNotFoundException() {
-        super();
+        super("해당하는 북마크를 찾을 수 없습니다.");
     }
 }
--- BE/house/src/main/java/com/nextsquad/house/exception/OauthClientNotFoundException.java
+++ BE/house/src/main/java/com/nextsquad/house/exception/OauthClientNotFoundException.java
@@ -2,6 +2,6 @@ package com.nextsquad.house.exception;
 
 public class OauthClientNotFoundException extends RuntimeException{
     public OauthClientNotFoundException() {
-        super("등록되어 있는 OauthClient가 아닙니다");
+        super("등록되어 있는 OAuth 서비스가 아닙니다");
     }
 }
