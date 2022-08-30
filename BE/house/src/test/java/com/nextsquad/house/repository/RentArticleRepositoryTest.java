cfb71c5f7dc4abf73e2b09a1e3c0d9b8f27b4ee6
Senglee Choi <tmdfl1027@gmail.com>
2022-08-30 15:51:07 +0900

    Test: RentArticleRepsotoryTest 작성
    - UserRepository는 Mockito를 사용해 모킹
    - 현재 RentArticle은 기본 생성자로 생성 후 reflection을 사용하는 방식. 추후 테스트용 RentArticle 클래스를 새로 정의하는 방식으로 변경 필요.

---
+++ BE/house/src/test/java/com/nextsquad/house/repository/RentArticleRepositoryTest.java
@@ -0,0 +1,211 @@
+package com.nextsquad.house.repository;
+
+import com.nextsquad.house.domain.house.RentArticle;
+import com.nextsquad.house.domain.user.User;
+import com.nextsquad.house.dto.SearchConditionDto;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class RentArticleRepositoryTest {
+    @Autowired
+    private RentArticleRepository rentArticleRepository;
+
+    private UserRepository mockedUserRepository;
+
+    private User user;
+
+    @TestConfiguration
+    static class TestConfig {
+        @PersistenceContext
+        private EntityManager em;
+        @Bean
+        public JPAQueryFactory jpaQueryFactory(){
+            return new JPAQueryFactory(em);
+        }
+    }
+
+    @BeforeEach
+    public void setUserRepository() {
+        User user = new User();
+        mockedUserRepository = Mockito.mock(UserRepository.class);
+        Mockito.when(mockedUserRepository.findById(1L)).thenReturn(Optional.of(user));
+    }
+
+    @Test
+    @DisplayName("양도글 작성 후 save()를 호출하면 저장 완료된 게시글을 반환해야 한다")
+    void addRentArticle() {
+        RentArticle article = new RentArticle();
+        RentArticle savedArticle = rentArticleRepository.save(article);
+        assertThat(savedArticle).isEqualTo(article);
+    }
+
+    @Test
+    @DisplayName("findById()를 호출하면 해당 ID에 맞는 게시글을 반환해야 한다")
+    void findRentArticleById() {
+        RentArticle article = new RentArticle();
+        rentArticleRepository.save(article);
+
+        RentArticle foundArticle = rentArticleRepository.findById(article.getId())
+                .orElse(new RentArticle());
+
+        assertThat(foundArticle).isEqualTo(article);
+    }
+
+    @Test
+    @DisplayName("findByUser()를 호출하면 해당 유저의 게시글을 반환해야 한다")
+    void findByUserTest() {
+        List<RentArticle> articles = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            RentArticle article = new RentArticle();
+            try {
+                Class articleClass = Class.forName("com.nextsquad.house.domain.house.RentArticle");
+                Field userField = articleClass.getDeclaredField("user");
+                userField.setAccessible(true);
+                userField.set(article, user);
+                assertThat(article.getUser()).isEqualTo(user);
+            } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
+                fail(e.getMessage());
+            }
+            articles.add(article);
+        }
+        rentArticleRepository.saveAll(articles);
+
+        PageRequest pageable = PageRequest.of(0, 5);
+        Page<RentArticle> pages = rentArticleRepository.findByUser(user, pageable);
+
+        List<RentArticle> collect = pages.get().collect(Collectors.toList());
+
+        for (int i = 0; i < 5; i++) {
+            assertThat(collect.get(i)).isEqualTo(articles.get(i));
+            assertThat(collect.get(i).getUser()).isEqualTo(user);
+        }
+
+        assertThat(pages.hasNext()).isTrue();
+        pageable = pageable.next();
+        pages = rentArticleRepository.findByUser(user, pageable);
+
+        collect = pages.get().collect(Collectors.toList());
+
+        for (int i = 0; i < 5; i++) {
+            assertThat(collect.get(i)).isEqualTo(articles.get(i + 5));
+            assertThat(collect.get(i).getUser()).isEqualTo(user);
+        }
+
+        assertThat(pages.hasNext()).isFalse();
+    }
+
+    @Test
+    @DisplayName("findByKeyword() 월세 순 정렬 테스트")
+    public void sortedByRentFeeTest() {
+        List<RentArticle> articles = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            RentArticle article = new RentArticle();
+            try {
+                Class articleClass = Class.forName("com.nextsquad.house.domain.house.RentArticle");
+                Field rentFeeField = articleClass.getDeclaredField("rentFee");
+                rentFeeField.setAccessible(true);
+                rentFeeField.set(article, 1000000 - (i * 1000));
+            } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
+                fail(e.getMessage());
+            }
+            articles.add(article);
+        }
+        rentArticleRepository.saveAll(articles);
+
+        SearchConditionDto condition = new SearchConditionDto(null, "rentFee", null);
+
+        int previousRentFee = Integer.MIN_VALUE;
+        PageRequest pageable = PageRequest.of(0, 10);
+
+        List<RentArticle> foundArticles = rentArticleRepository.findByKeyword(condition, pageable);
+        for (RentArticle foundArticle : foundArticles) {
+            assertThat(foundArticle.getRentFee()).isGreaterThanOrEqualTo(previousRentFee);
+            previousRentFee = foundArticle.getRentFee();
+        }
+    }
+
+    @Test
+    @DisplayName("findByKeyword() 보증금 순 정렬 테스트")
+    public void sortedByDepositTest() {
+        List<RentArticle> articles = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            RentArticle article = new RentArticle();
+            try {
+                Class articleClass = Class.forName("com.nextsquad.house.domain.house.RentArticle");
+                Field depositField = articleClass.getDeclaredField("deposit");
+                depositField.setAccessible(true);
+                depositField.set(article, 1000000 - (i * 1000));
+            } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
+                fail(e.getMessage());
+            }
+            articles.add(article);
+        }
+        rentArticleRepository.saveAll(articles);
+
+        SearchConditionDto condition = new SearchConditionDto(null, "deposit", null);
+
+        int previousDeposit = Integer.MIN_VALUE;
+        PageRequest pageable = PageRequest.of(0, 10);
+
+        List<RentArticle> foundArticles = rentArticleRepository.findByKeyword(condition, pageable);
+        for (RentArticle foundArticle : foundArticles) {
+            assertThat(foundArticle.getDeposit()).isGreaterThanOrEqualTo(previousDeposit);
+            previousDeposit = foundArticle.getDeposit();
+        }
+    }
+
+    @Test
+    @DisplayName("findByKeyword() 거래 가능만 보기 필터링 테스트")
+    public void filteredByAvailableOnlyTest() {
+        List<RentArticle> articles = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            RentArticle article = new RentArticle();
+            try {
+                Class articleClass = Class.forName("com.nextsquad.house.domain.house.RentArticle");
+                Field isCompletedField = articleClass.getDeclaredField("isCompleted");
+                isCompletedField.setAccessible(true);
+                isCompletedField.set(article, i > 5);
+            } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
+                fail(e.getMessage());
+            }
+            articles.add(article);
+        }
+        rentArticleRepository.saveAll(articles);
+
+        SearchConditionDto condition = new SearchConditionDto(true, null, null);
+
+        PageRequest pageable = PageRequest.of(0, 10);
+
+        List<RentArticle> foundArticles = rentArticleRepository.findByKeyword(condition, pageable);
+        assertThat(foundArticles.size()).isEqualTo(6);
+        for (RentArticle foundArticle : foundArticles) {
+            assertThat(foundArticle.isCompleted()).isFalse();
+        }
+
+        condition = new SearchConditionDto(false, null, null);
+        foundArticles = rentArticleRepository.findByKeyword(condition, pageable);
+        assertThat(foundArticles).isEqualTo(articles);
+    }
+}
