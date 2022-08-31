3bf1ee0d23634afa06d68177060369c7b6426eac
Senglee Choi <tmdfl1027@gmail.com>
2022-08-31 13:17:46 +0900

    Test: RentArticleRequest Builder 추가
    테스트 용이성을 위해 Request DTO에 빌더 추가

--- BE/house/src/main/java/com/nextsquad/house/dto/RentArticleRequest.java
+++ BE/house/src/main/java/com/nextsquad/house/dto/RentArticleRequest.java
@@ -1,11 +1,13 @@
 package com.nextsquad.house.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Builder
 @Getter
 @AllArgsConstructor
 public class RentArticleRequest {
