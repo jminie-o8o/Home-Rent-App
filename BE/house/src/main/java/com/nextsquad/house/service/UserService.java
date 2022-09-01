d2ec87048a7ddeb38629b153b8b7c84e6d8f84b0
Senglee Choi <tmdfl1027@gmail.com>
2022-09-01 20:58:09 +0900

    Fix: UserService @Transactional 어노테이션 변경
    javax.transaction에서 org.springframework.annotaion으로 변경

--- BE/house/src/main/java/com/nextsquad/house/service/UserService.java
+++ BE/house/src/main/java/com/nextsquad/house/service/UserService.java
@@ -16,8 +16,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
