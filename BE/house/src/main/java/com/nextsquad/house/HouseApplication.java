0dd464000a046bfa450dd2d00616f30f4a354d40
Senglee Choi <tmdfl1027@gmail.com>
2022-09-02 12:03:58 +0900

    Fix: 어플리케이션 시간대 변경
    Asia/Seoul로 변경

--- BE/house/src/main/java/com/nextsquad/house/HouseApplication.java
+++ BE/house/src/main/java/com/nextsquad/house/HouseApplication.java
@@ -3,8 +3,17 @@ package com.nextsquad.house;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class HouseApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HouseApplication.class, args);
