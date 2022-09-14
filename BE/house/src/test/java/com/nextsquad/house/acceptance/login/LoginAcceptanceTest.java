7b9b0204a606923d482647cb6b777a258f402824
Senglee Choi <tmdfl1027@gmail.com>
2022-09-14 11:01:16 +0900

    Test: 로그인 양수테스트 추가
    OauthClientMapper와 KakaoOauthClient를 모킹해 더미 유저의 userInfo를 리턴받도록 하여 구현.

---
+++ BE/house/src/test/java/com/nextsquad/house/acceptance/login/LoginAcceptanceTest.java
@@ -0,0 +1,95 @@
+package com.nextsquad.house.acceptance.login;
+
+import com.nextsquad.house.config.RestDocsConfiguration;
+import com.nextsquad.house.domain.user.User;
+import com.nextsquad.house.dto.OauthLoginRequestDto;
+import com.nextsquad.house.exception.UserNotFoundException;
+import com.nextsquad.house.login.jwt.JwtProvider;
+import com.nextsquad.house.login.jwt.JwtToken;
+import com.nextsquad.house.login.oauth.*;
+import com.nextsquad.house.login.userinfo.UserInfo;
+import com.nextsquad.house.repository.UserRepository;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.restassured3.RestAssuredRestDocumentation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+
+@Import(RestDocsConfiguration.class)
+@AutoConfigureRestDocs
+@ExtendWith({RestDocumentationExtension.class, MockitoExtension.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+public class LoginAcceptanceTest {
+    private RequestSpecification spec;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private JwtToken token;
+
+    private User user;
+
+    @MockBean
+    private OauthClientMapper oauthClientMapper;
+
+    @BeforeEach
+    void setup(RestDocumentationContextProvider restDocumentation) {
+        user = new User("testId", "testName", "lucas.com", OauthClientType.KAKAO);
+        token = jwtProvider.createJwtToken(userRepository.findById(1L).orElseThrow(UserNotFoundException::new));
+        RestAssured.port = port;
+        spec = new RequestSpecBuilder()
+                .addFilter(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Test
+    @DisplayName("Oauth 서버에서 발급받은 authCode로 로그인을 요청하면 OK 응답이 온다.")
+    void registerUserTest() {
+        KakaoOauthClient kakaoClient = Mockito.mock(KakaoOauthClient.class);
+        Mockito.when(kakaoClient.getUserInfo("authCode")).thenReturn(new UserInfo(user.getAccountId(), user.getDisplayName(), user.getProfileImageUrl(), user.getOauthClientType()));
+        Mockito.when(oauthClientMapper.getOauthClient("KAKAO")).thenReturn(Optional.of(kakaoClient));
+
+        OauthLoginRequestDto requestDto = new OauthLoginRequestDto("authCode", "KAKAO");
+
+        RestAssured
+            .given(spec)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .filter(RestAssuredRestDocumentation.document("get-user-info", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
+                .header("access-token", token.getAccessToken().getTokenCode())
+                .body(requestDto)
+            .when()
+                .post("/login/oauth")
+            .then()
+                .statusCode(HttpStatus.OK.value())
+                .log();
+    }
+}
