f266f203c03442ef3bb43d5da07e6ce11e1e3cb7
Senglee Choi <tmdfl1027@gmail.com>
2022-10-07 12:27:53 +0900

    refactor: @Valid 위반 시 발생하는 예외 Handler 처리
    MethodArgumentNotValidException의 내부 FieldError에서 defaultMessage를 꺼내 응답하도록 함.

--- BE/house/src/main/java/com/nextsquad/house/exception/RuntimeExceptionHandler.java
+++ BE/house/src/main/java/com/nextsquad/house/exception/RuntimeExceptionHandler.java
@@ -5,8 +5,12 @@ import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.nextsquad.house.dto.GeneralResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
 
 @RestControllerAdvice
 public class RuntimeExceptionHandler {
@@ -55,4 +59,13 @@ public class RuntimeExceptionHandler {
     public ResponseEntity<GeneralResponseDto> handleAccessDeniedException(AccessDeniedException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new GeneralResponseDto(401, e.getMessage()));
     }
+
+    @ExceptionHandler(value  = MethodArgumentNotValidException.class)
+    public ResponseEntity<GeneralResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        FieldError fieldError = Optional.ofNullable(e.getBindingResult().getFieldError())
+                .orElseGet(() -> new FieldError("Bad Request", "Error Field", "입력값을 확인하세요"));
+        String message = fieldError.getDefaultMessage();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new GeneralResponseDto(400, message));
+    }
+
 }
