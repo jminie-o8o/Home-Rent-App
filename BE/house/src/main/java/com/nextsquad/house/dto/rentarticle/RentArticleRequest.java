16c7aaa1306dca49628aa57f9b15d3dd3cf6f8f9
leekm0310 <leekm310@gmail.com>
2022-10-07 12:29:56 +0900

    refactor: RentArticleRequest 검증 어노테이션 추가
    - 필드 별로 검증 어노테이션 추가

--- BE/house/src/main/java/com/nextsquad/house/dto/rentarticle/RentArticleRequest.java
+++ BE/house/src/main/java/com/nextsquad/house/dto/rentarticle/RentArticleRequest.java
@@ -4,7 +4,11 @@ import com.nextsquad.house.domain.house.HouseFacility;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -14,24 +18,50 @@ import java.util.List;
 public class RentArticleRequest {
 
     private Long userId;
+
+    @NotBlank
+    @Length(max = 255, message = "주소는 255자 이내로 작성해주세요")
     private String address;
+
+    @NotBlank
+    @Length(max = 255)
     private String addressDetail;
+
     private String addressDescription;
+
     private double latitude;
     private double longitude;
+
+    @NotBlank
+    @Length(max = 255)
     private String title;
+
+    @NotBlank
     private String content;
+
+    @NotNull
     private String contractType;
+
+    @NotNull
     private String houseType;
+
     private int deposit;
     private int rentFee;
     private int maintenanceFee;
+
+    @NotBlank
     private String maintenanceFeeDescription;
+
+    @NotNull
     private LocalDate availableFrom;
+
+    @NotNull
+    @Future
     private LocalDate contractExpiresAt;
     private int maxFloor;
     private int thisFloor;
     private List<String> houseImages;
+    @NotNull
     private HouseFacilityListDto facility;
 
 
