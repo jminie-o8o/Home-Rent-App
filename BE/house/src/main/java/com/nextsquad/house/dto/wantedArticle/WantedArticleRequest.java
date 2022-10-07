c37565002b31b8785299a64173e7831aae5815c7
Senglee Choi <tmdfl1027@gmail.com>
2022-10-07 10:54:34 +0900

    refactor: WantedArticleRequest 필드 검증 어노테이션 추가

--- BE/house/src/main/java/com/nextsquad/house/dto/wantedArticle/WantedArticleRequest.java
+++ BE/house/src/main/java/com/nextsquad/house/dto/wantedArticle/WantedArticleRequest.java
@@ -3,18 +3,32 @@ package com.nextsquad.house.dto.wantedArticle;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
 public class WantedArticleRequest {
 
+    @NotNull
     private Long userId;
+
+    @NotBlank
     private String address;
+
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String content;
+
+    @NotNull
     private LocalDate moveInDate;
+
+    @NotNull
     private LocalDate moveOutDate;
+
     private int rentBudget;
     private int depositBudget;
 
