ece6fc6ca29d3ea8c2bd469f27ebb42550963442
Senglee Choi <tmdfl1027@gmail.com>
2022-10-13 11:24:15 +0900

    fix: BookmarkRequest 기본 생성자로 JSON 파싱 오류 해결

--- BE/house/src/main/java/com/nextsquad/house/dto/bookmark/BookmarkRequest.java
+++ BE/house/src/main/java/com/nextsquad/house/dto/bookmark/BookmarkRequest.java
@@ -2,11 +2,13 @@ package com.nextsquad.house.dto.bookmark;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookmarkRequest {
 
     @NotNull
