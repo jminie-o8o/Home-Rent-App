748b0cd66f924935aab30abb527b2b9ef40be6c9
leekm0310 <leekm310@gmail.com>
2022-11-28 11:13:24 +0900

    Feat: 엘라스틱 서치 의존성 추가 및 설정
    - build.gradle에 의존성 추가
    - ElasticSearchConfig 파일 생성

---
+++ BE/house/src/main/java/com/nextsquad/house/config/ElasticSearchConfig.java
@@ -0,0 +1,38 @@
+package com.nextsquad.house.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.mapping.model.FieldNamingStrategy;
+import org.springframework.data.mapping.model.SnakeCaseFieldNamingStrategy;
+
+@EnableElasticsearchRepositories
+@Configuration
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    public ElasticsearchOperations elasticsearchOperations(ElasticsearchConverter elasticsearchConverter, RestHighLevelClient elasticsearchClient) {
+        return new ElasticsearchRestTemplate(elasticsearchClient());
+    }
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Override
+    protected FieldNamingStrategy fieldNamingStrategy() {
+        return new SnakeCaseFieldNamingStrategy();
+    }
+}
