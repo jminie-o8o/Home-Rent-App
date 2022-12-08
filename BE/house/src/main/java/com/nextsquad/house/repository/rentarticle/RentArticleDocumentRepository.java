473a7bc9d368f26c41ea4907d2e28f195c26596a
leekm0310 <leekm310@gmail.com>
2022-12-08 12:15:36 +0900

    Feat: 엘라스틱 서치 검색 도입
    - RentArticleDocumentRepository 에서 쿼리 사용해 검색
    - rentArticle 검색 시에 엘라스틱 서치 DB 검색 후에 아이디들로 RDB에서 검색해서 반환하도록 수정

--- BE/house/src/main/java/com/nextsquad/house/repository/rentarticle/RentArticleDocumentRepository.java
+++ BE/house/src/main/java/com/nextsquad/house/repository/rentarticle/RentArticleDocumentRepository.java
@@ -2,11 +2,13 @@ package com.nextsquad.house.repository.rentarticle;
 
 import com.nextsquad.house.domain.house.RentArticleDocument;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
 
 public interface RentArticleDocumentRepository extends ElasticsearchRepository<RentArticleDocument, Long> {
 
-    List<RentArticleDocument> findByTitle(String title, Pageable pageable);
+    @Query("{\"bool\": {\"must\": [{\"match\": {\"title\" : \"?0\"}},{\"match\": {\"is_completed\": \"?1\"}},{\"match\": {\"is_deleted\": \"false\"}}]}}")
+    List<RentArticleDocument> findByTitle(String title, boolean available, Pageable pageable);
 }
