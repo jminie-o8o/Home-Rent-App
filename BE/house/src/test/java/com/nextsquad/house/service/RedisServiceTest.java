6697a8cf0415310833bde83e3a11a8237d71d31a
Senglee Choi <tmdfl1027@gmail.com>
2022-12-20 12:05:47 +0900

    test: RedisService 북마크/조회수 카운트 증가 테스트 작성

---
+++ BE/house/src/test/java/com/nextsquad/house/service/RedisServiceTest.java
@@ -0,0 +1,30 @@
+package com.nextsquad.house.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class RedisServiceTest {
+    @Autowired
+    private RedisService redisService;
+
+    @Test
+    public void incrementTest() {
+        assertThat(redisService.get("CountTest::" + 1L)).isNull();
+
+        redisService.increment("CountTest::" + 1L);
+        assertThat(redisService.get("CountTest::" + 1L)).isEqualTo("1");
+
+        redisService.increment("CountTest::" + 1L);
+        assertThat(redisService.get("CountTest::" + 1L)).isEqualTo("2");
+
+        redisService.delete("CountTest::" + 1L);
+
+        assertThat(redisService.get("CountTest::" + 1L)).isNull();
+    }
+
+
+}
